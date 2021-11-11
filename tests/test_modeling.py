c2b2bfa9520933adbd5b610722e6d9a293204556
Alexey Vatolin <vatolinalex@gmail.com>
2021-11-12 01:07:20 +0300

    Apply on_start event before test run

--- tests/test_modeling.py
+++ tests/test_modeling.py
@@ -1,17 +1,11 @@
-import sys
-
-from fastapi.testclient import TestClient
-
-sys.path.append("..")
-from service.main import app
-
-client = TestClient(app)
-
-
-def test_fit():
+def test_fit(client):
     response = client.post("/fit", json={"texts": []})
     assert response.status_code == 200
+
     response_json = response.json()
-    # assert len(response.json()) == 1
     assert "model_id" in response_json
     assert "version" in response_json and response_json["version"] == 1
+    assert "predictions" in response_json
+    assert len(response_json["predictions"]["topics"]) == len(
+        response_json["predictions"]["probabilities"]
+    )
