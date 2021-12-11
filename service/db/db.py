0b0cfbba28d8bf180f608fabe16229e736cbfd5a
AlexeyVatolin <vatolinalex@gmail.com>
2021-12-11 20:50:56 +0300

    Alembic migrations (#15)
    * Database migrations using alembic
    
    * Fix github action
    
    * Fix github action
    
    * Fix migration

--- service/db/db.py
+++ service/db/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import create_engine
 
 from service.core.config import settings
 from service.models import models  # NOQA
@@ -12,9 +12,3 @@ db_url = (
 
 engine_async = create_async_engine(db_url)
 engine = create_engine(db_url.replace("+asyncpg", ""))
-
-
-async def init_db():
-    async with engine_async.begin() as conn:
-        # await conn.run_sync(SQLModel.metadata.drop_all)
-        await conn.run_sync(SQLModel.metadata.create_all)
